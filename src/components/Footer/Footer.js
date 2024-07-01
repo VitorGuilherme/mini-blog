d1c3e718e97ee3c603aa1e3b1ba7de9ac09bc77d
VitorGuilherme <alfredo.neg9@gmail.com>
2024-07-01 12:13:07 -0300

    my first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <h3>Rodapé</h3>
+      <p>Mini Blog &copy; 2024</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpk9xzp8SuvvckfzL6LDQ9-cpGj49xd5U",
+  authDomain: "mini-blog-c48a8.firebaseapp.com",
+  projectId: "mini-blog-c48a8",
+  storageBucket: "mini-blog-c48a8.appspot.com",
+  messagingSenderId: "218877636693",
+  appId: "1:218877636693:web:25c4df6867f0aca68e64dc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {db};
