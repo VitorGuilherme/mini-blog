093fdb7d1e7c176aa12fae828fcccf555e275478
VitorGuilherme <alfredo.neg9@gmail.com>
2024-07-03 19:19:00 -0300

    SignUp and Login Screen created, authentication with email and password with firebase

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from "react";
 
+import styles from "./Footer.module.css";
+
 const Footer = () => {
   return (
-    <footer>
-      <h3>Rodapé</h3>
+    <footer className={styles.footer}>
       <p>Mini Blog &copy; 2024</p>
     </footer>
   );
